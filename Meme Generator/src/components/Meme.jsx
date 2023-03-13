22d015dba4b0b475cb2484554c2964e12c90aaea
alexandrunst <nastase.alexandru100@gmail.com>
2023-03-13 22:56:13 +0000

    Top and Bottom Text on state

--- Meme Generator/src/components/Meme.jsx
+++ Meme Generator/src/components/Meme.jsx
@@ -17,17 +17,39 @@ export default function Meme() {
         setMeme(prevMeme => ({ ...prevMeme, randomImage: randomMeme.url }));
     }
 
+    function handleChange(event) {
+        const { name, value } = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+
     return (
         <section className="meme">
             <div className="form" action="">
                 <div className="meme--inputs">
-                    <input type="text" name="topText" id="topText" placeholder="Top text" />
-                    <input type="text" name="bottomText" id="bottomText" placeholder="Bottom text" />
+                    <input
+                        type="text"
+                        name="topText"
+                        id="topText"
+                        placeholder="Top text"
+                        value={meme.topText}
+                        onChange={handleChange} />
+                    <input
+                        type="text"
+                        name="bottomText"
+                        id="bottomText"
+                        placeholder="Bottom text"
+                        value={meme.bottomText}
+                        onChange={handleChange} />
                 </div >
                 <button type="submit" onClick={getMemeImage}>Get a new meme image 🖼</button>
             </div>
             <div className="meme--img--wrapper">
                 <img className="meme--img" src={meme.randomImage} alt="Meme Image" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
             </div>
         </section>
     )
